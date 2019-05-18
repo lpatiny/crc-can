442bd8784a1179f0be92e791ecbef2b3593ea5a2
Luc Patiny <luc@patiny.com>
2019-05-18 20:01:47 +0200

    add one test

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -12,4 +12,13 @@ describe('CRC test', () => {
     expect(array).toStrictEqual([49, 50, 51, 52, 53, 54, 55, 56, 57]);
     expect(crc(array)).toBe(0x29b1);
   });
+
+  it('123', () => {
+    let string = '123';
+    let array = [];
+    for (let i = 0; i < string.length; i++) {
+      array.push(string.charCodeAt(i));
+    }
+    expect(crc(array)).toBe(0x5bce);
+  });
 });
